00fe6ce7a8c3fef8504c688347ad63b60f28eb6a
Mackinlay Kevin <kemack83@gmail.com>
2023-10-10 23:34:04 -0300

    instalacion y configuracion de Firebase 1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBiyG9WLUx1mhintCncxTQQYnf_FSKFYu0",
+  authDomain: "tienda-sports.firebaseapp.com",
+  projectId: "tienda-sports",
+  storageBucket: "tienda-sports.appspot.com",
+  messagingSenderId: "925919581688",
+  appId: "1:925919581688:web:8028aecc77f0303d80f20d",
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
