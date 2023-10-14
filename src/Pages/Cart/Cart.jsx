dafbcfdbdcd3639abd5e1d17ffb70e78990bf919
Mackinlay Kevin <kemack83@gmail.com>
2023-10-14 01:01:02 -0300

    fireBase, orders, buyers

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,23 +1,51 @@
-import { collection, getFirestore } from "firebase/firestore";
-import React from "react";
+import { addDoc, collection, doc, getFirestore, updateDoc, writeBatch } from "firebase/firestore";
+import React, { useContext, useState } from "react";
 import { items } from "../../Helpers/Items";
+import { CartContext } from "../../Components/Context/CartContext";
 
 const Cart = () => {
-  const crearOrden = () => {
-    const order = {
+  const { productsCarrito, eliminarProduct } = useContext(CartContext);
+  const [nombre, setNombre] = useState('kevin')
+  const [email, setEmail] = useState('kevin@gmail.com')
+  const [numero, setNumero] = useState(15409387)
+  const dataBase = getFirestore();
+  const ordersCollection = collection(dataBase, "orders");
+
+  const nuevaOrden = () => {
+    const total = productsCarrito.reduce((acum, item) => acum + item.precio, 0);
+    const orderData = {
       buyer: {
         name: "Carlos",
         number: 15308455,
         email: "carlos@gmail.com",
       },
-      items: [
-        { name: "producto 1", price: 1000 },
-        { name: "producto 2", price: 1000 },
-      ],
-      Total: 2000,
+      items: [...productsCarrito],
+      Total: total,
     };
-    const db = getFirestore();
-    const orderCollection = collection(db, "orders");
+
+    addDoc(ordersCollection, orderData).then(({ id }) => console.log(id));
+  };
+
+  const actualizarOrden = () => {
+    const orderDocument = doc(ordersCollection, "3Okqf7N6nrnrEpkqYx6a");
+    updateDoc(orderDocument, { total: 3000 });
+    console.log("orden actualizada");
+  };
+
+  const actualizarOrdenPorLotes = () => {
+    const Documento1 = doc(ordersCollection, "3Okqf7N6nrnrEpkqYx6a");
+    const Documento2 = doc(ordersCollection, "G5XuXtLjZdqPaOzpRrPv");
+    const batch = writeBatch(dataBase);
+
+    batch.update(Documento1, { total: 2000 });
+    batch.set(Documento2, {
+      buyer: {
+        name: "Jose",
+        number: 15443335,
+        email: "Joses@gmail.com",
+      },
+    });
+    batch.commit();
   };
 
   return (
@@ -25,9 +53,21 @@ const Cart = () => {
       <div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
         <h2>Cart</h2>
       </div>
+
+      <div style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+        <input type={"text"} onChange={(e) => setNombre(e.target.value)} placeholder="Nombre"></input>
+        <input type={Number} onChange={(e) => setNumero(e.target.value)} placeholder="Numero"></input>
+        <input type={"text"} onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
+      </div>
       <div style={styles.buttonContainer}>
-        <button style={styles.Products} onClick={crearOrden}>
+        <button style={styles.Products} onClick={nuevaOrden}>
           Comprar
+        </button>
+        <button style={styles.Products} onClick={actualizarOrden}>
+          Actualizar
+        </button>
+        <button style={styles.Products} onClick={actualizarOrdenPorLotes}>
+          Actualizar
         </button>
       </div>
     </>
@@ -53,9 +93,9 @@ const styles = {
     textDecoration: "none",
     backgroundColor: "blue",
   },
-  buttonContainer:{
-    display:"flex",
-    flexDirection:"row",
-    justifyContent:"center"
-  }
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
 };
